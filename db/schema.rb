ef51b1398af6376c6b4ec5bd43507167fc9b9b41
MenendezON <menendezon@gmail.com>
2023-11-25 22:36:23 +0000

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_25_180339) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_25_222656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "author_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_trades_on_author_id"
+    t.index ["category_id"], name: "index_trades_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "User", null: false
@@ -27,4 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_25_180339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "trades", "categories"
+  add_foreign_key "trades", "users", column: "author_id"
 end
