8479672339f37e9944d8f49e70fd165b22e01855
MenendezON <menendezon@gmail.com>
2023-11-28 12:51:16 +0000

    Fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   has_many :trades, foreign_key: 'author_id', dependent: :destroy
   has_many :categories, foreign_key: 'user_id', dependent: :destroy
-       
+
   validates :name, presence: true
-       
 end
--- spec/views/trades/index.html.erb_spec.rb
+++ spec/views/trades/index.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "trades/index.html.erb", type: :view do
+RSpec.describe 'trades/index.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
