9bf8688f3e9371d46bfdcae1e913861c7c5c10eb
MenendezON <menendezon@gmail.com>
2023-11-28 12:12:31 +0000

    Update categories migrate

--- db/migrate/20231125222413_create_categories.rb
+++ db/migrate/20231125222413_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
-      t.string :name
+      t.string :name, null: false, default: 'Unknown_category'
       t.string :icon
       t.references :user, null: false, foreign_key: true
 
