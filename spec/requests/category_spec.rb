53b6129bc109aa27c6a7d965611b81d1acd14164
MenendezON <menendezon@gmail.com>
2023-11-28 12:04:54 +0000

    Generate the spec folder and his subfolders

---
+++ spec/requests/category_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Categories", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/category/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/category/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/category/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
